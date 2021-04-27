5a3a1a6fb6f0056267d21065797c1593cfbbff7b
TangYunTao <597555230@qq.com>
2021-04-27 19:09:41 +0800

    测试配置

--- include/ejabberd_mqtt.hrl
+++ include/ejabberd_mqtt.hrl
@@ -7,5 +7,5 @@
 %%% Created : 22. 4月 2021 2:16 下午
 %%%-------------------------------------------------------------------
 -author("tangyuntao").
--define(CLIENT_ID_POSTFIX, "@1RK24W").
--define(REPLACE_PASSWORD, <<"$t$YWMtbOC6dpHQEeuCZ9WKo2iaqegrzF8zZk2Wp8GS3pF-orBnUI9QkdAR66aBgQQ44eDgAwMAAAF4hk55OgBPGgCFL7_cBzHctitvxpLeq8VDd54SeZ4yxF4cM2uvEV-nJA">>).
+-define(CLIENT_ID_POSTFIX, "@uoaa80").
+-define(REPLACE_PASSWORD, <<"$t$YWMtzlKx0KcnEeu6QUPxfhl3W7RUpqCJz0DinB6g6AW6mbKjrVFgpycR656mNdOkuZRaAwMAAAF5EitI8gBPGgBGVXbLyaYxL86sU3L6sLxccBqzEwVVlfRKOs0eIFJ3ag">>).
