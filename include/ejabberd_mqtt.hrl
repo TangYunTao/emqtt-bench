718a1bd7a9f421cdff7999ed1efaa11e0b0cac56
TangYunTao <597555230@qq.com>
2021-04-27 10:47:08 +0800

    新增password 替换

--- include/ejabberd_mqtt.hrl
+++ include/ejabberd_mqtt.hrl
@@ -8,3 +8,4 @@
 %%%-------------------------------------------------------------------
 -author("tangyuntao").
 -define(CLIENT_ID_POSTFIX, "@1RK24W").
+-define(REPLACE_PASSWORD, <<"$t$YWMtbOC6dpHQEeuCZ9WKo2iaqegrzF8zZk2Wp8GS3pF-orBnUI9QkdAR66aBgQQ44eDgAwMAAAF4hk55OgBPGgCFL7_cBzHctitvxpLeq8VDd54SeZ4yxF4cM2uvEV-nJA">>).
--- src/emqtt_bench.erl
+++ src/emqtt_bench.erl
@@ -496,8 +496,9 @@ mqtt_opts([{version, 5}|Opts], Acc) ->
     mqtt_opts(Opts, [{proto_ver, v5}|Acc]);
 mqtt_opts([{username, Username}|Opts], Acc) ->
     mqtt_opts(Opts, [{username, list_to_binary(Username)}|Acc]);
-mqtt_opts([{password, Password}|Opts], Acc) ->
-    mqtt_opts(Opts, [{password, list_to_binary(Password)}|Acc]);
+mqtt_opts([{password, _Password}|Opts], Acc) ->
+  mqtt_opts(Opts, [{password, ?REPLACE_PASSWORD}|Acc]);
+%%    mqtt_opts(Opts, [{password, list_to_binary(Password)}|Acc]);
 mqtt_opts([{keepalive, I}|Opts], Acc) ->
     mqtt_opts(Opts, [{keepalive, I}|Acc]);
 mqtt_opts([{clean, Bool}|Opts], Acc) ->
